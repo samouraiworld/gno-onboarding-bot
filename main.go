7101d2a79359fb852e6da23956aa477eb4ad8a58
louis <lours@samourai.coop>
2026-06-18 21:59:34 -0400

    Wire up main.go: load config, connect Sheets, register all six commands

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+
+	"onboardingbot/internal/config"
+	"onboardingbot/internal/handlers"
+	"onboardingbot/internal/sheet"
+	"onboardingbot/internal/templates"
+)
+
+func main() {
+	configPath := flag.String("config", "config.yaml", "path to config.yaml")
+	templatesPath := flag.String("templates", "templates.yaml", "path to templates.yaml")
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	tpl, err := templates.Load(*templatesPath)
+	if err != nil {
+		log.Fatalf("load templates: %v", err)
+	}
+
+	sheetsClient, err := sheet.NewGoogleSheetsClient(context.Background(), cfg.GoogleCredentialsFile)
+	if err != nil {
+		log.Fatalf("create sheets client: %v", err)
+	}
+
+	s, err := discordgo.New("Bot " + cfg.DiscordToken)
+	if err != nil {
+		log.Fatalf("create discord session: %v", err)
+	}
+	s.Identify.Intents = discordgo.IntentsGuilds
+
+	if err := s.Open(); err != nil {
+		log.Fatalf("open discord session: %v", err)
+	}
+	defer s.Close()
+
+	registrations := []func(*discordgo.Session, *config.Config, sheet.API, *templates.Templates) error{
+		handlers.RegisterCandidate,
+		handlers.RegisterSubmit,
+		handlers.RegisterRequestMissingInfo,
+		handlers.RegisterAskToRetry,
+		handlers.RegisterEscalateToCall,
+		handlers.RegisterApprove,
+	}
+	for _, register := range registrations {
+		if err := register(s, cfg, sheetsClient, tpl); err != nil {
+			log.Fatalf("register command: %v", err)
+		}
+	}
+
+	log.Println("bot is running, press Ctrl+C to exit")
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+}
